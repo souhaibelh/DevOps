cde4a2549844a8e697b30d1cf02d721438d22a11
Souhaib El Hassouni <61610@etu.he2b.be>
2025-08-23 20:01:23 +0200

    Update DemoApplication.java

--- service-java/src/main/java/be/esi/devops/demo/DemoApplication.java
+++ service-java/src/main/java/be/esi/devops/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Testing caching");
+		System.out.println("Testing cachinga");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 

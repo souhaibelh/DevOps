b317fb77500a3a09da61ece655dac9ba529b0a7b
Souhaib <judojudani@gmail.com>
2025-08-23 19:26:34 +0200

    Added teraform file that creates the azure infrastructure needed to hold both containers in the same network so they can communicate with each other

---
+++ ac.tf
@@ -0,0 +1,55 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "a8930602-911f-4a4f-9ca1-d48690c726b8"
+}
+
+resource "azurerm_resource_group" "rg" {
+    name = "microservices-rg"
+    location = "West Europe"
+}
+
+resource "azurerm_container_group" "microservices" {
+    name = "microservices-group"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    os_type = "Linux"
+    ip_address_type = "Public"
+
+    image_registry_credential {
+        server   = "index.docker.io"
+        username = "souhaibhassouni"
+        password = "dckr_pat_UmF4NyGgXosfY6cg-OC3CVJ2qUI"
+    }
+
+    container {
+        name = "springboot"
+        image = "souhaibhassouni/spring-boot-app:1.0"
+        cpu = "0.5"
+        memory = "1.5"
+
+        ports {
+            port = 8080
+            protocol = "TCP"
+        }
+
+        environment_variables = {
+            FLASK_URL = "http://localhost:5000"
+        }
+    }
+
+    container {
+        name = "flask"
+        image = "souhaibhassouni/flask:1.0"
+        cpu = "0.5"
+        memory = "1.5"
+
+        ports {
+            port = 5000
+            protocol = "TCP"
+        }
+    }
+
+    tags = {
+        project = "microservices"
+    }
+}

21c7f99377df0c489f53bfa9ab79147f2ea2f879
Souhaib El Hassouni <61610@etu.he2b.be>
2025-08-23 23:00:40 +0200

    Update app.py

--- service-python/app.py
+++ service-python/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/api/message")
 def message():
     # adding comment to test if job will run for asddd-asd
-    return jsonify({"message": "Hello from Flask!"})
+    return jsonify({"message": "Hello from Flaskyyy!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

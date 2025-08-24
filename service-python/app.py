367e6021adb8c6d212ebe3474dc759080cca128e
Souhaib El Hassouni <61610@etu.he2b.be>
2025-08-24 09:22:27 +0200

    Update app.py

--- service-python/app.py
+++ service-python/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/api/message")
 def message():
     # adding comment to test if job will rsssun foaaaar asddd-asd
-    return jsonify({"message": "Hello from Flaskyyy!"})
+    return jsonify({"message": "Hello from Flaskyyys!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

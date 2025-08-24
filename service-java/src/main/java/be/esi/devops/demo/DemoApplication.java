a37144625e4d7a9ec1970e7aa7dfd77f63163454
Souhaib El Hassouni <61610@etu.he2b.be>
2025-08-24 21:12:21 +0200

    Update DemoApplication.java

--- service-java/src/main/java/be/esi/devops/demo/DemoApplication.java
+++ service-java/src/main/java/be/esi/devops/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Testingdd fffassa");
+		System.out.println("Testingdd ffddfassa");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
